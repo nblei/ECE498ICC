343154d5470a8cf4f73ab0a3fc894630a51424ad
Nathan Bleier <nbleier3@illinois.edu>
2019-04-29 08:57:40 -0500

    Printing fake data

--- Client/client.py
+++ Client/client.py
@@ -9,7 +9,7 @@ UPPER=100
 LOWER=50
 TIMEOUT=1.0
 
-fake_data = {"Table1" : {None, None, {"name":"patch1", "value":73}, {"name":"patch2", "value":121} } }
+fake_data = {"Table1" : [None, None, {"name":"patch1", "value":73}, {"name":"patch2", "value":121} ] }
 
 def get_sensor_data_dict(host):
 	url = "{host}/data".format(host=host)
@@ -35,28 +35,49 @@ def get_color(val_0_255):
 		return 'r'
 	return 'g'
 
-def graph_data(data_dict):
+def graph_data(data_dict, fig, ax):
 	assert(data_dict is not None)
 	ngraphs = len(data_dict)
-	nvpairs = {}
+	rnames = []
+	rdata = []
+	rcolors = []
 	for name, data in data_dict.items():
 		for datum in data:
 			if datum is None:
 				continue
-			nvpairs[name+datum['name']] = (datum['value'] / 255.0, get_color(datum['value']))
+			pname = "{n}.{d}".format(n=name,d=datum['name'])
+			rnames.append(pname)
+			rdata.append(datum['value'] / 255.0)
+			rcolors.append(get_color(datum['value']))
+			#nvpairs.append([pname, datum['value'] / 255.0, get_color(datum['value'])])
 
-	print(nvpairs)
+	print(rnames)
+	print(rdata)
+	print(rcolors)
 
+	if (fig is None or ax is None):
+		fig, ax = plt.subplots()
+
+	locations = [x+1 for x in range(len(rdata))]
+	plt_list = plt.bar(locations, rdata)
+	for idx, artist in enumerate(plt_list):
+		artist.set_facecolor(rcolors[idx])
+
+	ax.set_xticks(locations)
+	ax.set_xticklabels(rnames)
+	plt.show()
+	return fig, ax
 
 
 
 if __name__ == "__main__":
+	input = ""
 	if len(argv) != 2:
 		print("usage: {file} <http server>".format(file=argv[0]))
+		input = "172.16.182.16"
 		exit(1)
-
-	host = "http://{ip}:1880".format(ip=argv[1])
+	else:
+		input = argv[1]
+	host = "http://{ip}:1880".format(ip=input)
 	print("Host: ", host)
-	graph_data(fake_data)
-
-
+	graph_data(fake_data, None, None)
